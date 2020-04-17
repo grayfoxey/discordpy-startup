c8ebc66aa87bf55556acdf1b005a7dee8005cd73
grayfoxey <63857797+grayfoxey@users.noreply.github.com>
2020-04-17 22:44:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,17 +5,43 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+client = commands.Bot(command_prefix='.')
+voice_client = None
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.command()
+async def join(ctx):
+    #voicechannelを取得
+    vc = ctx.author.voice.channel
+    #voicechannelに接続
+    await vc.connect()
+
+@client.command()
+async def bye(ctx):
+    #切断
+    await ctx.voice_client.disconnect()
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('.'):
+        pass
+
+    else:
+        if message.guild.voice_client:
+            print(message.content)
+            creat_WAV(message.content)
+            source = discord.FFmpegPCMAudio("output.wav")
+            message.guild.voice_client.play(source)
+        else:
+            pass
 
 
 bot.run(token)
